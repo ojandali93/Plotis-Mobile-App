a3398e6476768a3fe4781d44491ae33ce545bec6
omar jandali <omarjandali93@gmail.com>
2022-06-27 04:34:01 -0700

    addecd the initial structure for the recommended section of the application

--- plotis-mobile/src/components/RecommendedPropertyTile.js
+++ plotis-mobile/src/components/RecommendedPropertyTile.js
@@ -1,16 +1,68 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { Dimensions } from 'react-native'
 
-const RecommendedPropertyTile = () => {
+const RecommendedPropertyTile = (props) => {
+
+  const {
+    RecommendedDetailStack
+  } = props
+
+  let deviceWidth = Dimensions.get('window').width - 16
+  let tileWidth = deviceWidth * .55
+  let imageWidth = tileWidth - 8
+  let imageHeight = (tileWidth / 1.78) + 1
+
   return (
-    <View>
-      <Text>Tile Item</Text>
+    <View style={[styles.mainContainer, {width: tileWidth}]}>
+      <TouchableOpacity onPress={() => {RecommendedDetailStack()}}>
+        <View style={styles.tileContainer}>
+          <View style={[styles.ImageContainer, {height: imageHeight, width: tileWidth}]}>
+            <Image style={styles.mainImage} source={require('../../assets/luxury-home-1.jpeg')}/>
+          </View>
+          <View style={styles.priceContainer}>
+            <Text>$1,233,223</Text>
+            <Text>For Sale</Text>
+          </View>
+          <View>
+            <Text>1234 Bristol Ave.</Text>
+            <Text>Orange, CA 92956</Text>
+          </View>
+          <View>
+            <Text>3 Bed | 3 Bath | 1,234 Sqft</Text>
+          </View>
+          <View style={styles.priceContainer}>
+            <Text>Net Operating Income:</Text>
+            <Text>$1,223</Text>
+          </View>
+          <View style={styles.priceContainer}>
+            <Text>Cash On Cash Return:</Text>
+            <Text>6.6%</Text>
+          </View>
+          <View style={styles.priceContainer}>
+            <Text>Return On Investment:</Text>
+            <Text>10%</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  mainContainer: {
+    marginRight: 16,
+    paddingVertical: 8
+  },
+  mainImage: {
+    width: '100%',
+    height: '100%',
+  },
+  priceContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 })
 
 export default RecommendedPropertyTile
--- plotis-mobile/src/screens/RecommendedScreen.js
+++ plotis-mobile/src/screens/RecommendedScreen.js
@@ -1,31 +1,59 @@
 import React from 'react'
 import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler';
 import RecommendedPropertyTile from '../components/RecommendedPropertyTile';
 
-const RecommendedScreen = () => {
+const RecommendedScreen = ({navigation}) => {
 
   const properties = [{item:1}, {item:2}, {item:3}]
 
+  const RecommendedDetailStack = () => {
+    navigation.navigate('RecommendedDetailStack')
+  }
+
   const renderItem = ({ item }) => (
-    <RecommendedPropertyTile></RecommendedPropertyTile>
+    <RecommendedPropertyTile stytle={styles.tile} RecommendedDetailStack={RecommendedDetailStack}/>
   );
 
   return (
     <View>
-      <View className="sectionContainer">
-        <Text>Recommended Properties</Text>
-        <FlatList 
-          horizontal
-          data={properties}
-          renderItem={renderItem}
-        />
-      </View>
+      <ScrollView>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.categoryTitle}>Recommended Properties</Text>
+          <FlatList 
+            horizontal
+            data={properties}
+            renderItem={renderItem}
+          />
+        </View>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.categoryTitle}>Recommended Properties</Text>
+          <FlatList 
+            horizontal
+            data={properties}
+            renderItem={renderItem}
+          />
+        </View>
+        <View style={styles.sectionContainer}>
+          <Text style={styles.categoryTitle}>Recommended Properties</Text>
+          <FlatList 
+            horizontal
+            data={properties}
+            renderItem={renderItem}
+          />
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  sectionContainer: {
+    padding: 8
+  },
+  categoryTitle: {
+    paddingVertical: 8 
+  }
 })
 
 export default RecommendedScreen
